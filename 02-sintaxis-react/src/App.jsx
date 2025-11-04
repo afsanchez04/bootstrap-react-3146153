832f7d713c5dfddab7f5f1656353c04779652a42
afsaanchez04 <afsanchez04@gmail.com>
2025-11-04 13:03:15 -0500

    seguimiento actualizado

--- 02-sintaxis-react/src/App.jsx
+++ 02-sintaxis-react/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import "./App.css"
 import { NavBar } from './components/NavBar'
+import { ExtensionCard } from './components/ExtensionCard'
 
 export const App = () => {
   return (
     <>
       <NavBar/>
+      <ExtensionCard/>
     </>
   )
 }
---
+++ 02-sintaxis-react/src/components/ExtensionCard.jsx
@@ -0,0 +1,9 @@
+import "../stylesheets/ExtensionCard.css"
+
+export const ExtensionCard = () => {
+  return (
+    <>
+      <h1 className="title">Card Extension</h1>
+    </>
+  )
+}
