36ca4e9cd529aa18d54c5bcbf7dab182dbb4ed6f
afsaanchez04 <afsanchez04@gmail.com>
2025-11-12 12:20:29 -0500

    presesntación actualizada

--- 02-sintaxis-react/src/App.jsx
+++ 02-sintaxis-react/src/App.jsx
@@ -62,7 +62,19 @@ export const App = () => {
       <h1>React + Bootstrap</h1>
       <LoginForm />
 
-      <ProductCard/>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-6 col-lg-4">
+            <ProductCard/> 
+          </div>
+          <div className="col-12 col-md-6 col-lg-4 mt-4 mt-md-0">
+            <ProductCard/> 
+          </div>
+          <div className="col-12 col-md-12 col-lg-4 mt-4 mt-lg-0">
+            <ProductCard/> 
+          </div>
+        </div>
+      </div>
     </>
   );
 };
